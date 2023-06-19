54f25afa43151da01f50c748403ecfe42751ad94
ikechukwueze <themedric@live.com>
2023-06-19 23:45:49 +0100

    wip

---
+++ code/backend/webapp/vidmem/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class VideoMem(models.Model):
+    video_name = models.CharField(max_length=100)
+    video = models.FileField()
+    caption = models.TextField()
+    score = models.FloatField()
+
+    def __str__(self) -> str:
+        return self.video_name
