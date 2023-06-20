5e3f937c522936f4606b5f3067a03140ea163e97
ikechukwueze <themedric@live.com>
2023-06-20 22:47:06 +0100

    added default for score

--- code/backend/webapp/vidmem/models.py
+++ code/backend/webapp/vidmem/models.py
@@ -7,7 +7,7 @@ class VideoMem(models.Model):
     video_name = models.CharField(max_length=100)
     video = models.FileField()
     caption = models.TextField()
-    score = models.FloatField()
+    score = models.FloatField(default=0)
 
     def __str__(self) -> str:
         return self.video_name
